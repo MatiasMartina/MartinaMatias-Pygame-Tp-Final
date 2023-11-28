86a84e7eef4ae8aaa8e8e7c7e720fabb2f85c058
MatiasMartinaVivas <matiasmartina97@gmail.com>
2023-11-28 16:30:07 -0300

    Tercer commit: Solucionamos salto. Ahora salta y tiene animación. Cuando está en caida hace animación de caida, cuando está inactivo está en stay (iddle) Me resta solucionar como hacer para ver la animación completa del disparo y que no se superponga la inactividad cuando no se presiona ninguna tecla. Por otro lado agregué la clase bullet, logré hacer que dispare, ahora voy a trabajar con las colisiones y avanzar con las plataformas

---
+++ bullet.py
@@ -0,0 +1,54 @@
+# bullet.py
+
+import pygame as pg
+from models.constantes import DEBUG
+import math
+
+class Bala(pg.sprite.Sprite):
+    def __init__(self, owner, x_init, y_init, x_end, y_end, speed:100, frame_rate_ms, move_rate_ms, width=5, height=5):
+        super().__init__()
+
+        self.tiempo_transcurrido_move = 0
+        self.original_image = pg.image.load("assets\img\player\spell\\bullet.png").convert()
+        
+        #self.image.fill((255, 0, 0))  # Color rojo para la bala (puedes cambiarlo)
+        self.rect = pg.Rect(x_init, y_init, width + 40, height + 40)
+        self.image = pg.transform.scale(self.original_image, (self.rect.width, self.rect.height))
+        self.x = x_init
+        self.y = y_init
+        self.owner = owner
+        self.rect.x = x_init
+        self.rect.y = y_init
+        self.frame_rate_ms = frame_rate_ms
+        self.move_rate_ms = move_rate_ms
+        
+        
+
+        self.move_x = speed
+        self.move_y = 0
+
+        self.is_active = True
+
+    def update(self, delta_ms, plataform_list, enemy_list, player):
+        self.tiempo_transcurrido_move += delta_ms
+        if self.tiempo_transcurrido_move >= self.move_rate_ms:
+            self.tiempo_transcurrido_move = 0
+            
+            self.rect.x += self.move_x
+            self.rect.y += self.move_y
+            self.check_impact(plataform_list, enemy_list, player)
+
+    def check_impact(self, plataform_list, enemy_list, player):
+        if self.is_active and self.owner != player and self.rect.colliderect(player.rect):
+            print("IMPACTO PLAYER")
+            player.receive_shoot()
+            self.is_active = False
+        for aux_enemy in enemy_list:
+            if self.is_active and self.owner != aux_enemy and self.rect.colliderect(aux_enemy.rect):
+                print("IMPACTO ENEMY")
+                self.is_active = False
+    def draw(self, screen):
+        if self.is_active:
+            if DEBUG:
+                pg.draw.rect(screen, color=(255, 0, 0), rect=self.rect)
+            screen.blit(self.image, self.rect)
--- models/player/main_player.py
+++ models/player/main_player.py
@@ -1,10 +1,11 @@
 import pygame as pg
 from models.auxiliar import SurfaceManager as sf
 from models.constantes import ANCHO_VENTANA, ALTO_VENTANA
-
+from bullet import *
 
 class Jugador:
-    def __init__(self, coordenada_x , coordenada_y, frame_rate = 100, speed_walk = 6, speed_run = 12, gravity = 5, jump = 50):
+    lista_balas = []
+    def __init__(self, coordenada_x , coordenada_y, frame_rate = 1000, speed_walk = 6, speed_run = 12, gravity = 5, jump = 120):
 
         self.__iddle_r = sf.get_surface_from_sprisheet(".\\assets\\img\\player\\idle\\idle.png",6,1,) 
         self.__iddle_l = sf.get_surface_from_sprisheet(".\\assets\\img\\player\\idle\\idle.png",6,1, flip = True)
@@ -14,6 +15,9 @@ class Jugador:
         self.__jump_l = sf.get_surface_from_sprisheet(".\\assets\\img\\player\\jump\\Jump.png",11,1, flip = True)
         self.__run_r = sf.get_surface_from_sprisheet(".\\assets\\img\\player\\run\\Run.png",8,1,)
         self.__run_l = sf.get_surface_from_sprisheet(".\\assets\\img\\player\\run\\Run.png",8,1, flip = True)
+        self.__shot_r = sf.get_surface_from_sprisheet(".\\assets\\img\\player\\shot\\shot.png", 7, 1)
+        self.__shot_l = sf.get_surface_from_sprisheet(".\\assets\\img\\player\\shot\\shot.png" ,7, 1, flip=True)
+
         self.__move_x = coordenada_x  
         """¡¡ATENCION!! NO CONFUNDIR MOVE_X CON COORDENADAS DE APARICION"""
         self.__move_y = coordenada_y
@@ -25,27 +29,35 @@ class Jugador:
         self.__player_animation_time = 0
         self.__gravity = gravity #Cuantos pixeles de GRAVEDAD
         self.__jump = jump   #cuantos pixeles de salto
-        self.__is_jumping = False
-        self.__on_ground = False
         
-        self.tiempo_referencia = pg.time.get_ticks()
-        self.tiempo_entre_actualizaciones = 1000
+        self.__state = "Stay"
 
+        self.__reference_time = pg.time.get_ticks()
+        self.__time_between_updates = 1500
+        
 
         self.__initial_frame = 0 #Frame inicial que queremos tomar 
         self.__actual_animation = self.__iddle_r #Tomamos la lista entera de surfaces. Contiene cada frame de la animacion
         self.__actual_image_animation = self.__actual_animation[self.__initial_frame] #Definimos cual va a ser el frame de toda la lista que comience la animacion
         self.__rect = self.__actual_image_animation.get_rect() #Utilizamos el metodo get_rect para definir el limite de nuestra img, para x ejemplo, después poder trabajar con las colisiones
+        
+        self.__is_shoting = False
+        self.__is_jumping = False
+        self.__on_ground = False
         self.__is_looking_right = True 
         self.lifes = 5
         self.score = 0
-
+        self.__stay = True
     def actualizar_si_paso_segundo(self):
-        tiempo_actual = pg.time.get_ticks()
+        actual_time = pg.time.get_ticks()
 
-        if tiempo_actual - self.tiempo_referencia > self.tiempo_entre_actualizaciones:
-            self.tiempo_referencia = tiempo_actual
+        if actual_time - self.__reference_time > self.__time_between_updates:
+            self.__reference_time = actual_time
             # Tu código para ejecutar después de que haya pasado un segundo
+            return False
+        else:
+            
+            return True
             print("Ha pasado un segundo.")
 
     def __set_x_animations_preset(self, move_x, animation_list: list[pg.surface.Surface], look_r:bool):
@@ -69,7 +81,6 @@ class Jugador:
         '''
         self.__actual_animation = animation_list
         self.__is_looking_right = look_r
-
     def __gravity_force(self):
         if self.__rect.y < (ALTO_VENTANA - self.__actual_image_animation.get_height()) - 100:
             self.__is_jumping = True
@@ -79,11 +90,7 @@ class Jugador:
             self.__is_jumping = False
             self.__on_ground = True
             return 0
-
-    
-
-
-    def __set_and_animations_preset_y(self):
+    def __set_and_animations_preset_y(self, move_y,animation_list: list[pg.surface.Surface], look_r : bool):
         '''
         ¿Qué hace?
         El método '__set_and_animations_preset' permite modificar la posicion horiontal en el eje y del 'main_player'
@@ -93,7 +100,7 @@ class Jugador:
         ¿Qué devuelve?
         N/A
         '''
-        self.__move_y = -self.__jump
+        self.__move_y = move_y
         '''
         '__move_y' toma el valor del salto en negativo, puesto que controla el movimiento de la imagen de 'main_player'
         sobre el 'eje_y'
@@ -106,12 +113,14 @@ class Jugador:
         Caso contrario será negativo '-__speed_walk' ya que de esta forma se desplazará hacia la izquieda en el eje x
 
         '''
-        self.__actual_animation = self.__jump_r if self.__is_looking_right else self.__jump_l
+        self.__actual_animation = animation_list#self.__jump_r if self.__is_looking_right else self.__jump_l
         '''
         establece que la animación actual ('__actual_animation') es igual a la lista que almacena las sprites recortadas del salto der
         ('__jump_r')  siempre y cuando '__is_looking_right' sea True. Caso contrario será ('__jump_l')
         '''
         self.__initial_frame = 0
+        # if self.__initial_frame > len(self.__actual_animation) - 1:
+        #     self.__actual_animation = self.__iddle_r if self.__is_looking_right else self.__iddle_l
         '''
         Seteamos con valor inicial de todos los sprites el indice 0. O sea el primero de la lista en '__actual_animation'
         '''
@@ -120,12 +129,9 @@ class Jugador:
         Estado de salto en verdadero por que está saltando pero acá hay que cambiar algo, pues, salta y no puede quedar en verdadero, debe cambiar
         '''
         self.__on_ground = False
-        
-
-
-        
-
+  
     def walk(self, direction_walk: str = "Right"):
+        print("caminando")
         match direction_walk:
             case 'Right':
                 look_right = True
@@ -144,36 +150,122 @@ class Jugador:
                 else:
                     self.__set_x_animations_preset(-self.__speed_walk, self.__walk_l, look_r = look_right)
                     self.__rect.y += self.__gravity_force()
-
     def run(self, direction_walk: str = "Right"):
-        match direction_walk:
-            case 'Right':
-                look_right = True
-                self.__set_x_animations_preset(self.__speed_run, self.__run_r, look_r = look_right)
-            case 'Left':
-                look_right = False
-                self.__set_x_animations_preset(-self.__speed_run, self.__run_l, look_r = look_right)
+        print("Corriendo")
+        if self.__is_jumping:
+            self.__set_x_animations_preset(self.__speed_run, self.__run_r, look_r = True)
+        else:
 
+            match direction_walk:
+                case 'Right':
+                    look_right = True
+                    self.__set_x_animations_preset(self.__speed_run, self.__run_r, look_r = look_right)
+                case 'Left':
+                    look_right = False
+                    self.__set_x_animations_preset(-self.__speed_run, self.__run_l, look_r = look_right)
     def jump(self):
+        ("Saltando")
         if not self.__is_jumping and self.__on_ground:
-            self.__set_and_animations_preset_y()
-            print("Esta")
-            self.stop_jump()
+            if  self.__is_looking_right:
+                self.__set_and_animations_preset_y(-self.__jump, self.__jump_r, True)
+                print("EEEEEEEENTROOOOOOOOOOOO ACAAAAAAAAAAAAA")
+                
+            else:
+                self.__set_and_animations_preset_y(-self.__jump, self.__jump_l, False)
+                "Entró acá False"
+               
+       
+                  
+    # def stop_jump(self):
+    #     self.__is_jumping = False
+    #     self.__on_ground = True
+    #     self.__actual_animation = self.__iddle_r if self.__is_looking_right else self.__iddle_l
+    
+        
+        
+     
+    def shot(self):
             
-    def stop_jump(self):
-        self.__is_jumping = False
-        self.__on_ground = True
-        self.__actual_animation = self.__iddle_r if self.__is_looking_right else self.__iddle_l
-        
-        
-
+            self.__actual_animation = self.__shot_r if self.__is_looking_right else self.__shot_l
+            if self.__is_looking_right:
+                bullet = Bala(
+                owner = self,
+                x_init = self.__rect.x-50,
+                y_init = self.__rect.y+80,
+                x_end = 600,
+                y_end = 0,
+                speed = 100,
+                frame_rate_ms = 100,
+                move_rate_ms = 50)
+                self.lista_balas.append(bullet)
+            else:
+                bullet = Bala(
+                owner = self,
+                x_init = self.__rect.x+120,
+                y_init = self.__rect.y+80,
+                x_end = 0,
+                y_end = 0,
+                speed = -100,
+                frame_rate_ms = 100,
+                move_rate_ms = 50)
+                self.lista_balas.append(bullet)
+            # print("Disparando")
+            # if self.__is_jumping:
+            #     print("si salta")
+            #     self.__actual_animation = self.__shot_r if self.__is_looking_right else self.__shot_l
+            #     self.__initial_frame = 0
+            #     self.__is_shoting = False
+            # elif self.__on_ground:
+            #     print("si tierra")
+            #     self.__actual_animation = self.__shot_r if self.__is_looking_right else self.__shot_l
+            # else:
+                
+            #     self.__actual_animation =  self.__shot_r if self.__is_looking_right else self.__shot_l
+            
+            
     def stay(self):
-        if self.__actual_animation != self.__iddle_l and self.__actual_animation != self.__iddle_r:
-            self.__actual_animation = self.__iddle_r if self.__is_looking_right else self.__iddle_l
-            self.__initial_frame = 0
-            self.__move_x = 0
-            self.__move_y = 0
-
+        print("stay")
+        
+        if self.__on_ground:
+            if not (self.__is_jumping or self.__is_shoting):
+                self.__actual_animation = self.__iddle_r if self.__is_looking_right else self.__iddle_l
+                print("Quieto iddle")
+                self.__move_x = 0
+                self.__move_y = 0
+            
+            elif self.__is_shoting:
+                self.__actual_animation = self.__shot_r if self.__is_looking_right else self.__shot_l
+        else:
+            if not self.__on_ground and self.__is_shoting:
+                print("acá no entra mucho tiempo me pareceeeeeeeeeeeeeeeeeeeeeeee")
+                self.__actual_animation = self.__shot_r if self.__is_looking_right else self.__shot_l
+            else:
+                self.__actual_animation = self.__jump_r if self.__is_looking_right else self.__jump_l
+            
+        # if self.__on_ground:
+            
+        #     if self.__actual_animation not in [self.__iddle_l, self.__iddle_r, self.__shot_l, self.__shot_r, self.__jump_l, self.__jump_r]:
+        #         print("Pasó el primer if")
+        #         self.__actual_animation = self.__iddle_r if self.__is_looking_right else self.__iddle_l
+                
+        #         print("Quieto iddle")
+        #         self.__move_x = 0
+        #         self.__move_y = 0
+        #     elif self.__is_jumping:
+        #         print("acá siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii else if jumping if sino está en el piso entonces está volando")
+        #         self.__actual_animation = self.__jump_r if self.__is_looking_right else self.__jump_l
+        #     elif self.__is_shoting:
+        #         self.__actual_animation = self.__shot_r if self.__is_looking_right else self.__shot_l
+                
+        # else:
+        #     if not self.__on_ground and self.__is_shoting:
+        #         print("acá no entra mucho tiempo me pareceeeeeeeeeeeeeeeeeeeeeeee")
+        #         self.__actual_animation = self.__shot_r if self.__is_looking_right else self.__shot_l
+                
+        #     else:
+        #         self.__actual_animation = self.__jump_r if self.__is_looking_right else self.__jump_l
+                
+        
     def __set_edges_limits_x(self):
         '''
         ¿Qué hace?
@@ -208,42 +300,64 @@ class Jugador:
         return pixels_move_y
     
 
+
+
+
+
         
     def update_x_y(self):
         self.__rect.x += self.__set_edges_limits_x()
         if self.__rect.y >= (ALTO_VENTANA - self.__actual_image_animation.get_height())-100:
             self.__rect.y += self.__set_edges_limits_y()
+            print(f"Posicion eje y: {self.__rect.y}")
+            print(f"Posicion eje x: {self.__rect.x}")
         else: 
             self.__rect.y += 0;
     
     def do_movement(self, delta_ms):
         self.__player_move_time += delta_ms
+        print(f"Tiempo movimiento: {self.__player_move_time} : - Tope 100 frame")
         if self.__player_move_time >= self.__frame_rate:
+            print("REINICIO DE FRAME. SIGUIENTE MOVIMIENTO EN X E Y")
             self.__player_move_time = 0
+        self.__rect.y += self.__gravity_force()
         self.update_x_y()
-        if (self.__is_jumping): #Si está saltando, que deje de saltar y que su capacidad de movimiento sea 0 en eje y.
-                    self.__is_jumping = False
+        if (self.__is_jumping): #Si está saltando, que deje de saltar y que su capacidad de movimiento sea 0 en eje y y eje x
+                    # self.__is_jumping = False
                     self.__move_y = 0
                     self.__move_x = 0
-        self.__rect.y += self.__gravity_force()
+        
+        
+        
         self.actualizar_si_paso_segundo()
                
-
-    
-
     def do_animation(self, delta_ms):
         self.__player_animation_time += delta_ms
+        print(f"Tiempo de animación{self.__frame_rate}")
+        print(f"fotograma numero {self.__initial_frame} de {len(self.__actual_animation)}")
         if self.__player_animation_time >= self.__frame_rate:
+            print(f"REINICIO DE FRAME. SIGUIENTE ANIMACIÓN ")
             self.__player_animation_time = 0
-            if self.__initial_frame < len(self.__actual_animation) - 1:
-                self.__initial_frame += 1
-            else:
-                self.__initial_frame = 0
+            self.__initial_frame = (self.__initial_frame + 1) % len(self.__actual_animation)
+        #     if self.__initial_frame < len(self.__actual_animation) - 1:
+        #         print(self.__initial_frame)
+        #         print(len(self.__actual_animation))
+        #         self.__initial_frame += 1
+        #     else:
+        #         self.__initial_frame = 0
+        # print("actualizando animacion")
                 
-                
+    
+
                     
     
     def update(self, delta_ms):
+        print("Estados:")
+        print(f"En tierra: {self.__on_ground}")
+        print(f"Saltando: {self.__is_jumping}")
+        print(f"Disparando: {self.__is_shoting}" )
+        print(f"Mirando derecha: {self.__is_looking_right}")
+        print("UPDATE - MOVIMIENTO EN X - Y - FOTOGRAMAS ANIMACIÓN")
         self.do_animation(delta_ms)
         self.do_movement(delta_ms)
         self.actualizar_si_paso_segundo()
@@ -253,6 +367,8 @@ class Jugador:
         screen.blit(self.__actual_image_animation, self.__rect)
     
 
+    
+    
 
 
         
